ef1626b821a54d7744c6c9249ba744a8264d5c2c
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2021-06-21 19:34:34 +0800

    will no longer accidentally scan your entire system

--- main.nim
+++ main.nim
@@ -2,7 +2,17 @@
 import os
 import strutils 
 
-# echo getCurrentDir()
+echo currentSourcePath.parentDir()
+echo getCurrentDir()
+
+if getCurrentDir() & "/" == getHomeDir():
+	echo "\e[31m" & "\nCannot run this program from home directory, aborting\n" & "\e[0m"
+	echo "Executing this program from your home directory will have it scan your entire system."
+	echo "If you did not intend to execute it from your home folder, you may be using MacOS, in which case it is incorectly setting the working directory."
+	echo "To fix this, you should run the executable with a terminal, as it does not have the same problem."
+	quit(1)
+
+# assert getCurrentDir() & "/" != getHomeDir()
 
 for file in walkDirRec getCurrentDir():
 	let fileSplit = splitFile(file)
